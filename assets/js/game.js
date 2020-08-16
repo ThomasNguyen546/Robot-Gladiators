e871d3eec2ec6f2c0e83fb793befcc5ba2de7e32
Thomas Nguyen <thomasnguyen546@gmail.com>
2020-08-15 18:33:10 -0600

    bug player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -165,6 +165,18 @@ function randomNumber(min, max) {
   return value;
 }
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 var playerInfo = {
   name: window.prompt("What is your robot's name?"),
   health: 100,
